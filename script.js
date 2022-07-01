2c80ca6a62990d370750a5a1541cb98f570221a0
unknown <ancar-personal@LTC-0187.siiromania.ro>
2022-07-01 07:32:34 +0300

    first files for calculator

---
+++ script.js
@@ -0,0 +1,124 @@
+class Calculator {
+	constructor(previousOperandTextElement, currentOperandTextElement) {
+		this.previousOperandTextElement = previousOperandTextElement;
+		this.currentOperandTextElement = currentOperandTextElement;
+		this.clear();
+	}
+
+	clear() {
+		this.currentOperand = "";
+		this.previousOperand = "";
+		this.operation = undefined;
+	}
+
+	delete() {
+		this.currentOperand = this.currentOperand.toString().slice(0, -1);
+	}
+
+	// repeat toString, slice
+	appendNumber(number) {
+		if (number === "." && this.currentOperand.includes(".")) return;
+		this.currentOperand = this.currentOperand.toString() + number.toString();
+	}
+
+	// here is something wrong that has to be investigated, the operation sign is not appearing in the calculator
+	chooseOperation(operation) {
+		if (this.currentOperand === "") return;
+		if (this.previousOperand !== "") {
+			this.compute();
+		}
+		this.operation = operation;
+		this.previousOperand = this.currentOperand;
+		this.currentOperand = "";
+	}
+
+	compute() {
+		let computation;
+		const prev = parseFloat(this.previousOperand);
+		const current = parseFloat(this.currentOperand);
+		if (isNaN(prev) || isNaN(current)) return;
+		switch (this.operation) {
+			case "+":
+				computation = prev + current;
+				break;
+			case "-":
+				computation = prev - current;
+				break;
+			case "*":
+				computation = prev * current;
+				break;
+			case "÷":
+				computation = prev + current;
+				break;
+			default:
+				return;
+		}
+		this.currentOperand = computation;
+		this.operation = undefined;
+		this.previousOperand = "";
+	}
+
+	getDisplayNumber(number) {
+		return number;
+	}
+
+	updateDisplay() {
+		this.currentOperandTextElement.innerText = this.getDisplayNumber(
+			this.currentOperand
+		);
+		if (this.operation != null) {
+			this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+				this.previousOperand
+			)} ${this.operation}`;
+		}
+		this.previousOperandTextElement.innerText = this.previousOperand;
+	}
+}
+
+// read more about querySelector and querySelectorAll
+// find out why we put querySelectorAll("[data-operation]"); and not querySelectorAll(["data-operation"]);
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+	"[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+	"[data-current-operand]"
+);
+
+const calculator = new Calculator(
+	previousOperandTextElement,
+	currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		calculator.appendNumber(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+operationButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		calculator.chooseOperation(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+equalsButton.addEventListener("click", (button) => {
+	calculator.compute();
+	calculator.updateDisplay();
+});
+
+allClearButton.addEventListener("click", (button) => {
+	calculator.clear();
+	calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", (button) => {
+	calculator.delete();
+	calculator.updateDisplay();
+});
